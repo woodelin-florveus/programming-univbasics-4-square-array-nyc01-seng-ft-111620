9171eab243db10cef642730ff30104bef919bbce
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-26 15:28:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  # squares each element
+  # returns a new array of these squared numbers
+
+  counter = 0
+  new_arr = []
+  while counter < array.length
+    new_arr.push(array[counter] ** 2)
+    counter += 1
+  end
+  new_arr
 end
